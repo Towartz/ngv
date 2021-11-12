cc2aff00d9d3760f432de1c8b436cd2e26cdcd2a
Towartz <dofthgumyxd@gmail.com>
2021-11-12 14:27:11 +0700

    ngv

---
+++ message/help.js
@@ -0,0 +1,242 @@
+exports.gameMenu = (prefix) => {
+    return `
+ 🕹️ GAME-MENU 🕹️
+ 
+᪣ ${prefix}slot
+᪣ ${prefix}limitgame
+᪣ ${prefix}gelud @tag
+᪣ ${prefix}tictactoe @tag
+᪣ ${prefix}siapaaku
+᪣ ${prefix}family100
+᪣ ${prefix}kuismath
+᪣ ${prefix}asahotak
+᪣ ${prefix}tebaklirik
+᪣ ${prefix}tebaklagu
+᪣ ${prefix}tebakkata
+᪣ ${prefix}susunkata
+᪣ ${prefix}kimiakuis
+᪣ ${prefix}caklontong
+᪣ ${prefix}tebakjenaka
+᪣ ${prefix}tebakanime
+᪣ ${prefix}tebaktebakan
+᪣ ${prefix}tebakgambar
+᪣ ${prefix}tebakbendera
+᪣ ${prefix}suit *batu/kertas/gunting*
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+   # FUN-MENU
+
+᪣ ${prefix}mining
+᪣ ${prefix}togel
+᪣ ${prefix}cekwatak
+᪣ ${prefix}cekmati [nama]
+᪣ ${prefix}wangy [nama]
+᪣ ${prefix}citacita
+᪣ ${prefix}toxic
+᪣ ${prefix}truth
+᪣ ${prefix}dare
+᪣ ${prefix}apakah
+᪣ ${prefix}bisakah
+᪣ ${prefix}kapankah
+᪣ ${prefix}rate
+᪣ ${prefix}jadian
+᪣ ${prefix}cantik
+᪣ ${prefix}ganteng
+᪣ ${prefix}beban
+᪣ ${prefix}babi
+᪣ ${prefix}cekganteng
+᪣ ${prefix}cekcantik
+
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+  #  OTHER-MENU 
+
+᪣ ${prefix}brainly *query*
+᪣ ${prefix}shopee *product*
+᪣ ${prefix}playstore *query*
+᪣ ${prefix}ssweb *query*
+᪣ ${prefix}google *query*
+᪣ ${prefix}image *query*
+᪣ ${prefix}pinterest *query*
+᪣ ${prefix}nulis *teks*
+᪣ ${prefix}iguser *ussername*
+᪣ ${prefix}igstalk *username*
+᪣ ${prefix}githubstalk *username*
+᪣ ${prefix}tiktokstalk *ussername*
+᪣ ${prefix}img2url *reply foto*
+᪣ ${prefix}ytsearch *query*
+
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+  # STICKER-MENU
+
+᪣ ${prefix}dadu
+᪣ ${prefix}doge
+᪣ ${prefix}toimg
+᪣ ${prefix}patrick
+᪣ ${prefix}garwgura
+᪣ ${prefix}ttg *teks*
+᪣ ${prefix}attp *teks*
+᪣ ${prefix}stickeranime
+᪣ ${prefix}semoji *emoji*
+᪣ ${prefix}sticker *reply foto/video*
+᪣ ${prefix}smeme *teks|teks*
+᪣ ${prefix}swm *pack|author*
+᪣ ${prefix}take *pack|author* 
+᪣ ${prefix}tovideo *reply sgif*
+
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+  # WIBU-MENU
+
+᪣ ${prefix}loli
+᪣ ${prefix}manga
+᪣ ${prefix}anime 
+᪣ ${prefix}lolivideo
+᪣ ${prefix}husbu
+᪣ ${prefix}waifu
+᪣ ${prefix}milf
+᪣ ${prefix}neko
+᪣ ${prefix}kanna
+᪣ ${prefix}sagiri
+᪣ ${prefix}hentai
+᪣ ${prefix}cosplay
+᪣ ${prefix}wallnime
+᪣ ${prefix}kusonime
+᪣ ${prefix}megumin
+᪣ ${prefix}otakudesu
+᪣ ${prefix}doujindesu
+᪣ ${prefix}storyanime
+᪣ ${prefix}nakanomiku
+᪣ ${prefix}nakanonino
+᪣ ${prefix}nakanoitsuki
+᪣ ${prefix}otakuongoing
+᪣ ${prefix}nhentai *code*
+᪣ ${prefix}nekopoi *link*
+᪣ ${prefix}nekopoi3d
+᪣ ${prefix}nekopoicosplay
+᪣ ${prefix}nekopoisearch
+
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+  # OWNER-MENU
+
+᪣ ${prefix}bc *teks*
+᪣ ${prefix}term
+᪣ ${prefix}eval
+᪣ ${prefix}reset
+᪣ ${prefix}clearall
+᪣ ${prefix}leaveall
+᪣ ${prefix}join *link gc*
+᪣ ${prefix}shutdown
+᪣ ${prefix}getquoted
+᪣ ${prefix}addupdate *fiturnya*
+᪣ ${prefix}exif *nama|author*
+᪣ ${prefix}sewa add/del *waktunya*
+᪣ ${prefix}premium add @tag|nomor
+᪣ ${prefix}premium del @tag|nomor
+
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+  # GROUP-MENU
+
+᪣ ${prefix}groupsetting
+᪣ ${prefix}afk *alasan*
+᪣ ${prefix}ceksewa
+᪣ ${prefix}kickall
+᪣ ${prefix}infogrup
+᪣ ${prefix}promote
+᪣ ${prefix}demote
+᪣ ${prefix}listonline
+᪣ ${prefix}tagall *teks*
+᪣ ${prefix}leave
+᪣ ${prefix}kick *reply*
+᪣ ${prefix}add *+62xxxxxx*
+᪣ ${prefix}setgrupname
+᪣ ${prefix}setppgrup
+᪣ ${prefix}setdesc
+᪣ ${prefix}sider *reply chat bot*
+᪣ ${prefix}hidetag *teks/reply teks*
+,
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+  ⬇️DOWNLOAD-MENU⬇️
+
+᪣ ${prefix}fb 
+᪣ ${prefix}igdl 
+᪣ ${prefix}igdl2 
+᪣ ${prefix}twitter 
+᪣ ${prefix}tiktok 
+᪣ ${prefix}play 
+᪣ ${prefix}ythd 
+᪣ ${prefix}ytmp3 
+᪣ ${prefix}ytmp4 
+᪣ ${prefix}soundcloud 
+᪣ ${prefix}tiktoknowm 
+᪣ ${prefix}tiktokaudio
+᪣ ${prefix}mediafire 
+᪣ ${prefix}nhentaipdf *code*
+
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+
+ # RULES BOT
+
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+4. Gpp sc gw jelek yg penting 
+Bot gw jalan awokawoak
+Jika sudah dipahami rules-nya, silakan ketik *#menu* untuk memulai!
+
+Owner  BOTZ:
+wa.me/6283102176419
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+  # INFO-MENU
+
+᪣ ${prefix}update
+᪣ ${prefix}level
+᪣ ${prefix}rules
+᪣ ${prefix}profile
+᪣ ${prefix}waktu
+᪣ ${prefix}botstat
+᪣ ${prefix}sewabot
+᪣ ${prefix}listsewa
+᪣ ${prefix}owner
+᪣ ${prefix}ping
+᪣ ${prefix}runtime
+᪣ ${prefix}donasi
+᪣ ${prefix}leaderboard
+᪣ ${prefix}cekpremium
+᪣ ${prefix}listpremium
+᪣ ${prefix}sourcecode
+᪣ ${prefix}bugreport *keluhan*
+
+`
+} 
